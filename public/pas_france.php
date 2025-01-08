77ab1eb37ae6051b8f31ccb337ed7e45c82eac65
Alexis Carrot <alexis.carrot42700@gmail.com>
2025-01-08 14:42:35 +0100

    update pas_france.php

--- public/pas_france.php
+++ public/pas_france.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>IP étrangère</title>
+    <title>403 Forbidden</title>
     <meta charset='utf-8'>
     <link rel="stylesheet" type="text/css" href="css/pas_france.css"/>
 </head>
 <body>
     <div class="border">
         <div class="contains">
-            <h1>Vous n'êtes pas en France !</h1>
+            <h1>Vous n'êtes pas autoisé à accéder à cette page.</h1>
         </div>
     </div>
 </body>
